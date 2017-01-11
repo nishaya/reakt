b22236755c95d3ba0fac1424e5cb557aba3cab61
nishaya <nishaya@users.noreply.github.com>
2017-01-11 15:30:09 +0900

    basic files

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -3,6 +3,11 @@ import path from 'path'
 import config from 'config'
 
 module.exports = {
+  entry: './src/application.jsx',
+  output: {
+    path: './public',
+    filename: 'application.js',
+  },
   devServer: {
     contentBase: './public',
     port: config.WEBPACK_DEV_SERVER_PORT || 8080,
