a27bdf7c1b372a059cf4e802ac502307fea6b487
nishaya <nishaya@users.noreply.github.com>
2017-01-12 15:27:23 +0900

    render

--- src/application.jsx
+++ src/application.jsx
@@ -1,1 +1,6 @@
-console.log('hi')
+import React from 'react'
+import { render } from 'react-dom'
+
+render((<div>
+  hi!
+</div>), document.getElementById('app'))
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -8,6 +8,11 @@ module.exports = {
     path: './public',
     filename: 'application.js',
   },
+  module: {
+    loaders: [
+      { test: /\.jsx?$/, exclude: /node_modules/, loaders: ['babel'] },
+    ],
+  },
   devServer: {
     contentBase: './public',
     port: config.WEBPACK_DEV_SERVER_PORT || 8080,
