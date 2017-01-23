e7d615caa8802323acf534f754b47f9b1bd8a399
nishaya <nishaya@users.noreply.github.com>
2017-01-23 19:13:30 +0900

    move to component

--- src/application.jsx
+++ src/application.jsx
@@ -5,25 +5,9 @@ import IndexComponent from 'components/index'
 // http://www.material-ui.com/#/
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import RaisedButton from 'material-ui/RaisedButton';
-import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 
 injectTapEventPlugin();
 
-const style = {
-  marginLeft: 12,
-}
-
 render((<MuiThemeProvider>
-  <div>
-    <Card>
-      <CardHeader title="test app" subtitle="subtitle"/>
-      <CardText>
-        <RaisedButton onMouseDown={e => console.log(e)} label="ok" primary style={style} />
-        <RaisedButton label="ng" secondary style={style} />
-        <RaisedButton label="disabled" disabled style={style} />
-      </CardText>
-    </Card>
-    <IndexComponent />
-  </div>
+  <IndexComponent />
 </MuiThemeProvider>), document.getElementById('app'))
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,7 +1,20 @@
 import React, { Component } from 'react'
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import RaisedButton from 'material-ui/RaisedButton';
 
 export default class IndexComponent extends Component {
   render() {
-    return (<div>index</div>)
+    const style = {
+      marginLeft: 12,
+    }
+    
+    return (<Card>
+          <CardHeader title="test app" subtitle="subtitle"/>
+          <CardText>
+            <RaisedButton onMouseDown={e => console.log(e)} label="ok" primary style={style} />
+            <RaisedButton label="ng" secondary style={style} />
+            <RaisedButton label="disabled" disabled style={style} />
+          </CardText>
+        </Card>)
   }
 }
