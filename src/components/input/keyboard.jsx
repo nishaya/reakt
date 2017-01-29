dc7dd2f87b452de17c0524663204a4df8c9ebd5c
nishaya <nishaya@users.noreply.github.com>
2017-01-29 10:30:02 +0900

    using refs

--- src/components/input/keyboard.jsx
+++ src/components/input/keyboard.jsx
@@ -12,22 +12,32 @@ class KeyboardInput extends Component {
     keyDown: () => {},
   }
 
+
   constructor(props) {
     super(props)
     document.addEventListener('keydown', event => this.onKeyDown(event))
     this.state = {
       key: null,
     }
+    this.focus = this.focus.bind(this)
   }
 
   onKeyDown(event) {
     this.setState({ key: event.key })
     this.props.keyDown(event.key)
+    this.focus()
   }
+
+  focus() {
+    this.textInput.focus()
+  }
+
+  textInput = null
 
   render() {
     return (<div>
       pressed key: {this.state.key}
+      <input type="text" ref={(input) => { this.textInput = input }} />
     </div>)
   }
 }
