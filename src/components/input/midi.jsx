49d08ac8ef275aad68480e08230b9f23647e95eb
nishaya <nishaya@users.noreply.github.com>
2017-01-29 22:25:05 +0900

    midi input

---
+++ src/components/input/midi.jsx
@@ -0,0 +1,9 @@
+import React, { Component, PropTypes } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import InputActions from 'actions/input'
+
+class MidiInput extends Component {
+  static propTypes = {
+  }
+}
