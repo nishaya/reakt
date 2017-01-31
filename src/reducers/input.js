2b33422db217296e597471a46bfeb8c9dd41de2a
nishaya <nishaya@users.noreply.github.com>
2017-01-31 11:47:05 +0900

    reducer

--- src/reducers/input.js
+++ src/reducers/input.js
@@ -1,12 +1,16 @@
-import { INPUT_KEY_DOWN } from 'actions/action_types'
+import { INPUT_KEY_DOWN, INPUT_KEY_UP } from 'actions/action_types'
 import createReducer from './create_reducer'
 
 const initialState = {
+  keyUp: {},
   keyDown: {},
 }
 
 export default createReducer(initialState, {
+  [INPUT_KEY_UP](state, action) {
+    return { ...state, keyUp: { ...state.keyUp, [action.payload]: true } }
+  },
   [INPUT_KEY_DOWN](state, action) {
-    return { ...state, keyDown: { ...state.keyDown, [action.payload.key]: true } }
+    return { ...state, keyDown: { ...state.keyDown, [action.payload]: true } }
   },
 })
