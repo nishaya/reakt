d627e79c812bb9e6ecd98250a693bd9f88d38094
nishaya <nishaya@users.noreply.github.com>
2017-01-31 10:44:57 +0900

    using redux-actions

--- src/actions/input.js
+++ src/actions/input.js
@@ -1,12 +1,6 @@
+import { createAction } from 'redux-actions'
 import { INPUT_KEY_DOWN } from './action_types'
 
 export default {
-  keyDown: key => (
-    {
-      type: INPUT_KEY_DOWN,
-      payload: {
-        key,
-      },
-    }
-  ),
+  keyDown: createAction(INPUT_KEY_DOWN),
 }
