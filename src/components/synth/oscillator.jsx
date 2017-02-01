cc29fa9caac9e4b63d06f22f991b1f7d3b3f93bb
nishaya <nishaya@users.noreply.github.com>
2017-02-01 23:14:08 +0900

    visualize notes

--- src/components/synth/oscillator.jsx
+++ src/components/synth/oscillator.jsx
@@ -1,14 +1,35 @@
 import React, { Component, PropTypes } from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
+import Checkbox from 'material-ui/Checkbox'
 
 class OscillatorComponent extends Component {
+  static propTypes = {
+    noteOn: PropTypes.shape(),
+    controlChange: PropTypes.shape(),
+  }
+
+  static defaultProps = {
+    noteOn: [],
+    controlChange: [],
+  }
+
+  componentWillReceiveProps(nextProps) {
+    console.log(nextProps)
+  }
+
   render() {
-    return (<div>oscillator</div>)
+    return (<div>
+      {Object.keys(this.props.noteOn).map(note =>
+        (<div key={`note_${note}`} style={{ display: 'inline' }}>{this.props.noteOn[note] ? 'o' : '-'}</div>),
+      )}
+    </div>)
   }
 }
 
-const mapStateToProps = () => ({})
+const mapStateToProps = state => ({
+  noteOn: state.midi.noteOn,
+})
 
 const mapDispatchToProps = dispatch => bindActionCreators({}, dispatch)
 export default connect(
