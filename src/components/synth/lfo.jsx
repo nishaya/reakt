331b24175819b386228e306ea3c5bca3a6acb672
nishaya <nishaya@users.noreply.github.com>
2017-02-07 18:10:55 +0900

    on ready

--- src/components/synth/lfo.jsx
+++ src/components/synth/lfo.jsx
@@ -1,6 +1,15 @@
 import React, { Component, PropTypes } from 'react'
 
 export default class LFO extends Component {
+  static propTypes = {
+    audioCtx: PropTypes.instanceOf(AudioContext).isRequired,
+    onReady: PropTypes.func,
+  }
+
+  static defaultProps = {
+    onReady: (lfo) => { console.log(lfo) },
+  }
+
   render() {
     return (<div>LFO</div>)
   }
