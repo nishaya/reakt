46b32216c49871e0867531e604f9466aec776ba5
nishaya <nishaya@users.noreply.github.com>
2017-02-07 17:48:46 +0900

    add EG

--- __tests__/components/control/slider.test.jsx
+++ __tests__/components/control/slider.test.jsx
@@ -3,13 +3,13 @@ import { shallow } from 'enzyme'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import SliderControl from '../../../src/components/control/slider'
 
-test('Slider contains label text', () => {
-  const labelText = 'label text'
-  const wrapper = shallow(<MuiThemeProvider>
-    <SliderControl label={labelText} />
-  </MuiThemeProvider>)
-  const slider = wrapper.find(SliderControl).shallow()
+const labelText = 'label text'
+const wrapper = shallow(<MuiThemeProvider>
+  <SliderControl label={labelText} />
+</MuiThemeProvider>)
+const slider = wrapper.find(SliderControl).shallow()
 
+test('Slider contains label text', () => {
   expect(slider.find('h3').length).toEqual(1)
   expect(slider.find('h3').text()).toEqual(labelText)
 })
