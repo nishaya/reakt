9b2e8d816222f1c704457bc94e2e820d2e4a19d1
nishaya <nishaya@users.noreply.github.com>
2017-02-08 09:02:25 +0900

    osc component

---
+++ src/components/synth/oscillator.jsx
@@ -0,0 +1,42 @@
+import React, { Component, PropTypes } from 'react'
+
+export default class Oscillator extends Component {
+  static propTypes = {
+    audioCtx: PropTypes.instanceOf(AudioContext).isRequired,
+    type: PropTypes.string,
+  }
+
+  static defaultProps = {
+    onPlay: (frequency, node) => { console.log('onPlay', frequency, node) }
+    type: 'square',
+  }
+
+  static value2freq(value) {
+    return (value / 4) + 0.001
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      type: 'square',
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.type !== nextProps.type) {
+      this.type = nextProps.type
+    }
+  }
+
+  set type(type) {
+    this.lfo.type = type
+    this.setState({ type })
+  }
+
+  render() {
+    return (<div>
+      <h2>Oscillator</h2>
+      <div>type: {this.state.type}</div>
+    </div>)
+  }
+}
