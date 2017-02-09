cc167abc418a5a87b7273e8dadb9f5c3ed68a3cb
nishaya <nishaya@users.noreply.github.com>
2017-02-09 23:48:58 +0900

    persist selected device

--- src/application.jsx
+++ src/application.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { createStore, applyMiddleware } from 'redux'
+import { compose, createStore, applyMiddleware } from 'redux'
 import createLogger from 'redux-logger'
 import { Provider } from 'react-redux'
 import IndexComponent from 'components/index'
 import reducers from 'reducers/index'
+import persistState from 'redux-localstorage'
 
 // http://www.material-ui.com/#/
 import injectTapEventPlugin from 'react-tap-event-plugin'
@@ -13,12 +14,16 @@ import getMuiTheme from 'material-ui/styles/getMuiTheme'
 import reaktTheme from 'styles/reakt_theme'
 import applicationStyle from './application.scss'
 
+const createPersistentStore = compose(
+  persistState('midiInput'),
+)(createStore)
+
 injectTapEventPlugin()
 
 const middlewares = process.env.NODE_ENV === 'development' ?
   applyMiddleware(createLogger()) : applyMiddleware()
 
-const store = createStore(reducers, middlewares)
+const store = createPersistentStore(reducers, middlewares)
 
 render((<Provider store={store}>
   <MuiThemeProvider muiTheme={getMuiTheme(reaktTheme)}>
