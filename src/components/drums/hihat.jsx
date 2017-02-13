69e6db4449aaba2b617370ad00b68c08b6546bed
nishaya <nishaya@users.noreply.github.com>
2017-02-13 17:10:07 +0900

    display playing/stopped

--- src/components/drums/hihat.jsx
+++ src/components/drums/hihat.jsx
@@ -19,12 +19,18 @@ export default class Hihat extends Component {
     release: 0.3,
   }
 
+  state = { playing: false }
+
   componentDidMount() {
     this.props.onReady(this.trigger.bind(this))
     this.buffer = NoiseGenerator.generateWhiteNoise(this.props.audioCtx)
+    this.pt = null
   }
 
   trigger(velocity) {
+    if (this.pt) {
+      clearTimeout(this.pt)
+    }
     console.log('trigger hihat', velocity)
     const bufferSource = this.props.audioCtx.createBufferSource()
     const volume = velocity / 127
@@ -46,13 +52,17 @@ export default class Hihat extends Component {
     bufferSource.loop = true
     bufferSource.start()
     bufferSource.stop(now + attack + release)
+
+    this.setState({ playing: true })
+
+    this.pt = setTimeout(() => this.setState({ playing: false }), (release - now) * 1000)
   }
 
   render() {
     return (<div className="reakt-drum__container">
       <h3>Hihat</h3>
       <div>
-        hihat
+        {this.state.playing ? 'playing' : 'stopped'}
       </div>
     </div>)
   }
