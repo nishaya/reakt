e8168aaad6a90f3dc7336da619302c80dbb59456
nishaya <nishaya@users.noreply.github.com>
2017-02-15 00:09:40 +0900

    delay params

--- src/components/synth/effects/delay.jsx
+++ src/components/synth/effects/delay.jsx
@@ -6,39 +6,75 @@ export default class Delay extends Component {
   static propTypes = {
     audioCtx: PropTypes.instanceOf(AudioContext).isRequired,
     onReady: PropTypes.func,
-    amount: PropTypes.number,
-    time: PropTypes.number,
-    feedback: PropTypes.number,
   }
 
   static defaultProps = {
     onReady: (delay) => {
       console.log(delay)
     },
-    amout: 0.5,
-    time: 0.3461,
-    feedback: 0.5,
+  }
+
+  static PARAMS = {
+    amount: {
+      label: 'Amount',
+      min: 0,
+      max: 1.0,
+    },
+    time: {
+      label: 'DelayTime',
+      min: 0,
+      max: 5.0,
+    },
+    feedback: {
+      label: 'FeedbackLevel',
+      min: 0,
+      max: 1.0,
+    },
   }
 
   constructor(props) {
     super(props)
+    this.state = {
+      amount: 0.5,
+      time: 0.3461,
+      feedback: 0.5,
+    }
   }
 
   componentWillMount() {
     this.delay = new DelayEffect(
       this.props.audioCtx,
-      this.time,
-      this.amount,
-      this.feedback,
+      this.state.time,
+      this.state.amount,
+      this.state.feedback,
     )
     this.props.onReady(this.delay)
+  }
+
+  renderSlider(key) {
+    return (<div>
+      {Delay.PARAMS[key].label} : {parseInt(this.state[key] * 100, 10) / 100}
+      <Slider
+        key={`slider_${key}`}
+        min={Delay.PARAMS[key].min}
+        max={Delay.PARAMS[key].max}
+        step={0.01}
+        value={this.state[key]}
+        onChange={(e, value) => {
+          this.delay[key] = value
+          this.setState({ [key]: value })
+        }}
+      />
+    </div>)
   }
 
   render() {
     return (<div className="reakt-component__container">
       <h2>Delay</h2>
       <div className="reakt-component__body">
-        delay
+        {this.renderSlider('time')}
+        {this.renderSlider('amount')}
+        {this.renderSlider('feedback')}
       </div>
     </div>)
   }
--- src/models/effects/delay.js
+++ src/models/effects/delay.js
@@ -1,21 +1,24 @@
 export default class DelayEffect {
   constructor(audioCtx, time = 0.3461, volume = 0.5, feedback = 0.5) {
     this.delayNode = audioCtx.createDelay(10)
-    this.delayNode.delayTime.value = time
+    // this.delayNode.delayTime.value = time
+    this.time = time
     this.input = audioCtx.createGain()
     this.wet = audioCtx.createGain()
-    this.feedback = audioCtx.createGain()
+    this.feedbackNode = audioCtx.createGain()
 
-    this.wet.gain.value = volume
-    this.feedback.gain.value = feedback
+    // this.wet.gain.value = volume
+    this.amount = volume
+    // this.feedbackNode.gain.value = feedback
+    this.feedback = feedback
 
     this.output = audioCtx.createGain()
 
     this.input.connect(this.wet)
     this.wet.connect(this.delayNode)
     this.delayNode.connect(this.output)
-    this.delayNode.connect(this.feedback)
-    this.feedback.connect(this.delayNode)
+    this.delayNode.connect(this.feedbackNode)
+    this.feedbackNode.connect(this.delayNode)
     this.input.connect(this.output)
   }
 
@@ -29,6 +32,14 @@ export default class DelayEffect {
   }
 
   set time(time) {
-    this.delayNode.delayTime = time
+    this.delayNode.delayTime.value = time
+  }
+
+  set amount(value) {
+    this.wet.gain.value = value
+  }
+
+  set feedback(value) {
+    this.feedbackNode.gain.value = value
   }
 }
