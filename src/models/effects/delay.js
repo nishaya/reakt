fcf0bc99dfb56cf1a228dea8210e0ad75d567cd1
nishaya <nishaya@users.noreply.github.com>
2017-02-14 23:53:03 +0900

    delay feedback

--- src/models/effects/delay.js
+++ src/models/effects/delay.js
@@ -1,15 +1,21 @@
 export default class DelayEffect {
-  constructor(audioCtx, time = 0.3461, volume = 0.5) {
+  constructor(audioCtx, time = 0.3461, volume = 0.5, feedback = 0.5) {
     this.delayNode = audioCtx.createDelay(10)
     this.delayNode.delayTime.value = time
     this.input = audioCtx.createGain()
     this.wet = audioCtx.createGain()
+    this.feedback = audioCtx.createGain()
+
     this.wet.gain.value = volume
+    this.feedback.gain.value = feedback
+
     this.output = audioCtx.createGain()
 
     this.input.connect(this.wet)
     this.wet.connect(this.delayNode)
     this.delayNode.connect(this.output)
+    this.delayNode.connect(this.feedback)
+    this.feedback.connect(this.delayNode)
     this.input.connect(this.output)
   }
 
