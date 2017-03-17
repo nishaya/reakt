94eb4bf2b62abdc1135c0df7fcf88f0636883570
nishaya <nishaya@users.noreply.github.com>
2017-03-18 06:34:25 +0900

    drum base component

---
+++ src/components/drums/base.jsx
@@ -0,0 +1,24 @@
+import React, { Component, PropTypes } from 'react'
+import PausedIcon from 'material-ui/svg-icons/av/pause'
+import PlayingIcon from 'material-ui/svg-icons/av/play-arrow'
+
+export default class Base extends Component {
+  static propTypes = {
+    label: PropTypes.string,
+  }
+
+  static defaultProps = {
+    label: 'Base',
+  }
+
+  render() {
+    return (<div className="reakt-drum__container">
+      <h3>{this.props.label}</h3>
+      <div>
+        {this.state.playing ?
+          (<PlayingIcon style={{ color: 'rgb(0, 188, 212)' }} />) :
+          (<PausedIcon style={{ color: '#ccc' }} />)}
+      </div>
+    </div>)
+  }
+}
